cf18d045e03d41d55343f36af9eee9a3a6f8353e
featherplain <megumi@featherplain.com>
2019-01-14 21:43:30 +0900

    render Header && Footer

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,22 @@
-<template>
-  <h1>Hello world!</h1>
+<template lang="pug">
+  div
+    h1 {{ description }}
 </template>
+
+<script>
+import { description, author } from 'feathericon/package.json';
+
+export default {
+  layout: 'app',
+  data: function() {
+    return {
+      description: description
+    }
+  },
+  head () {
+    return {
+      title: 'feathericon'
+    }
+  }
+}
+</script>
