1551ea76f56df845fc707e01715c57725d9c583d
featherplain <megumi@featherplain.com>
2019-01-14 21:42:53 +0900

    add Header

---
+++ components/Header.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+  div
+    h1 feathericon
+    p v{{version}}
+</template>
+
+<script>
+import { version } from 'feathericon/package.json';
+
+export default {
+  data: function() {
+    return {
+      version: version
+    }
+  }
+}
+</script>
+
+<style lang="css">
+</style>
