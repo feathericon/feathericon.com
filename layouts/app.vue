2bdb9cf7dbc27b86865dc4a50ef54b90408de50e
featherplain <megumi@featherplain.com>
2019-01-14 21:42:17 +0900

    add pug-loader && layout

---
+++ layouts/app.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+  div
+    Header
+    nuxt
+    Footer
+</template>
+
+<script>
+import Header from '~/components/Header.vue';
+import Footer from '~/components/Footer.vue';
+
+export default {
+  components: {
+    Header,
+    Footer
+  }
+}
+</script>
